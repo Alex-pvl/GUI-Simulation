6509548c1c8ea3f0781a83dd415035524ab3b3f2
Alex-pvl <sasha.pavlyuk.1996@inbox.ru>
2022-02-19 19:43:35 +0700

    TODO: fix timer visual

--- JavaLabs/src/nstu/MyFrame.java
+++ JavaLabs/src/nstu/MyFrame.java
@@ -4,14 +4,18 @@ import nstu.vehicles.Vehicle;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.util.Timer;
 import java.util.TimerTask;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class MyFrame extends JFrame {
+    boolean willShowStats = false;
     boolean willShowTime = false;
+    boolean isStarted = false;
     Habitat h = new Habitat();
-    Timer timer = new Timer();
-    long time = 0;
+    AtomicLong timeS = new AtomicLong(0);
 
     public MyFrame() {
         super("Road");
@@ -20,37 +24,113 @@ public class MyFrame extends JFrame {
         Toolkit toolkit = Toolkit.getDefaultToolkit();
         Dimension dimension = toolkit.getScreenSize();
         setBounds(dimension.width/2 - h.WIDTH/2, dimension.height/2 - h.HEIGHT/2, h.WIDTH, h.HEIGHT);
-        timer.schedule(new MyTimerTask(), 100 ,1000);
-        setVisible(true);
-    }
+        setFocusable(true);
 
-    // параметр для таймера
-    class MyTimerTask extends TimerTask {
-        @Override
-        public void run() {
-            System.out.println("time = " + time);
-            drawVehicles();
-            h.update(time);
-            showTimer();
-            repaint();
-            setVisible(true);
-            time++;
-            repaint();
-        }
-    }
+        JLabel timeLabel = new JLabel();
+        timeLabel.setText("Time: " + timeS + " s");
+        timeLabel.setFont(new Font("JavaLabs/fonts/ttf/JetBrainsMono-Regular.ttf", Font.BOLD, 15));
+        timeLabel.setVerticalAlignment(JLabel.TOP);
+        timeLabel.setHorizontalAlignment(JLabel.CENTER);
+        timeLabel.setPreferredSize(new Dimension(10, 10));
+        timeLabel.setBackground(Color.BLACK);
+        timeLabel.setVisible(false);
+        add(timeLabel);
 
-    public void showTimer() {
-        add(new JComponent() {
+        JLabel stats = new JLabel();
+        stats.setFont(new Font("JavaLabs/fonts/ttf/JetBrainsMono-Regular.ttf", Font.ITALIC, 20));
+        stats.setVerticalAlignment(JLabel.CENTER);
+        stats.setHorizontalAlignment(JLabel.CENTER);
+        stats.setPreferredSize(new Dimension(100, 100));
+        stats.setBackground(Color.RED);
+        add(stats);
+
+        class MyTimerTask extends TimerTask {
+            private Habitat habitat;
+            private long time;
+
+            public MyTimerTask(Habitat habitat, long time) {
+                this.habitat = habitat;
+                this.time = time;
+            }
+
             @Override
-            protected void paintComponent(Graphics g) {
-                String timeS = "Time: " + (time-1) + " s";
-                Font font = new Font("JavaLabs/fonts/ttf/JetBrainsMono-Regular.ttf", Font.BOLD, 15);
-                g.setFont(font);
-                g.setColor(Color.BLACK);
-                g.drawString(timeS, 2, 15);
-                setVisible(true);
+            public void run() {
+                timeS.set(timeS.get()+1);
+                System.out.println("time = " + time);
+                habitat.update(time);
+                time++;
+                drawVehicles();
+                timeLabel.setText("Time: " + timeS + " s");
+            }
+        }
+
+
+
+
+        this.addKeyListener(new KeyAdapter() {
+            Timer timer;
+            long time;
+            MyTimerTask task = null;
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                switch (e.getKeyChar()) {
+                    case 'b':
+                        if (!isStarted) {
+                            repaint();
+                            if (willShowStats) {
+                                stats.setVisible(false);
+                                willShowStats = false;
+                            }
+
+                            //h.vehicles.clear();
+                            isStarted = true;
+                            h.carCount = 0;
+                            h.motoCount = 0;
+                            timer = new Timer();
+                            time = 0;
+                            timeS.set(0);
+                            task = new MyTimerTask(h, time);
+                            timer.schedule(task, 0, 1000);
+                        }
+                        break;
+                    case 'e':
+                        if (isStarted) {
+                            timer.cancel();
+                            isStarted = false;
+                        }
+                        if(!willShowStats) {
+                            if (willShowTime) {
+                                timeLabel.setVisible(false);
+                                willShowTime = false;
+                            }
+
+                            stats.setText("<html><br>Simulation time: " + timeS + " s</br>" +
+                                    "<br>Total vehicles: " + h.vehicles.size() + "</br>" +
+                                    "<br>Cars number: " + h.carCount + "</br>" +
+                                    "<br>Motorbikes number: " + h.motoCount + "</br></html>");
+                            stats.setVisible(true);
+                            repaint();
+                            willShowStats = true;
+                        }
+                        break;
+                    case 't':
+                        if (!willShowStats) {
+                            if (!willShowTime) {
+                                timeLabel.setVisible(true);
+                                willShowTime = true;
+                            } else {
+                                timeLabel.setVisible(false);
+                                willShowTime = false;
+                            }
+                        }
+                        break;
+                    default: break;
+                }
             }
         });
+
+        setVisible(true);
     }
 
     public void drawVehicles() {
---
+++ JavaLabs/src/nstu/Repainter.java
@@ -0,0 +1,10 @@
+package nstu;
+
+import java.util.TimerTask;
+
+public class Repainter extends TimerTask {
+    @Override
+    public void run() {
+
+    }
+}
